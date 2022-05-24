d2c29cf85f61ef7940b24e7df67bb414b30bd121
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-05-24 01:03:07 -0300

    fix(index): it fix the getObjectWithLinkFromFile function to work with relative paths;

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 const fs = require("fs");
-const path = require("path");
 const chalk = require("chalk");
-
-// function getFile(filePath) {
-//   fs.readFile(filePath, "utf-8", (error, data) => {    
-//     if (error) throw new Error(chalk.redBright(error));
-
-//     console.log(chalk.greenBright(data));
-//   });  
-// }
-
-// function getFile(filePath) {
-//   fs.promises.readFile(filePath, "utf-8")
-//     .then(file => console.log(chalk.greenBright(file)))
-//     .catch(error => console.log(chalk.redBright(error.message)));
-// }
-
-// getFile("arquivos/texto.md");
 
 function extractLinks(text) {
   const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
@@ -30,19 +13,9 @@ function extractLinks(text) {
   return links.length === 0 ? "no links found" : links;
 }
 
-// async function getFile(filePath) {
-//   try {
-//     const text = await fs.promises.readFile(filePath, "utf-8");
-//     return extractLinks(text);  
-//   } catch (error) {
-//     return  chalk.red(error);    
-//   }
-// }
-
 async function getObjectsWithLinkFromFile(filePath) {
   try {
-    const absolutePath = path.join(__dirname, filePath);
-    const text = await fs.promises.readFile(absolutePath, "utf-8");
+    const text = await fs.promises.readFile(filePath, "utf-8");
     
     return extractLinks(text);
   } catch (error) {
